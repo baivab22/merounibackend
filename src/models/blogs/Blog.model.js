5b44e7c30740109e0811983cbc4ff6da8199a043
Saroj Dahal <isarojdahal@gmail.com>
2026-02-04 13:09:45 +0545

    fixes in blogs API (remove visiblitiy; can be done from status)

--- src/models/blogs/Blog.model.js
+++ src/models/blogs/Blog.model.js
@@ -1,91 +1,87 @@
 import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../../config/database.config.js";
 
-class Blog extends Model { }
+class Blog extends Model {}
 
 Blog.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-            allowNull: false,
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        slug: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        category: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: "categories",
-                key: "id",
-            },
-            onDelete: "CASCADE",
-        },
-        tags: {
-            type: DataTypes.JSON,
-            allowNull: false,
-            defaultValue: [],
-        },
-        description: {
-            type: DataTypes.TEXT,
-            allowNull: true,
-        },
-        content: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        featuredImage: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        author: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: "mu_users",
-                key: "id",
-            },
-            onDelete: "CASCADE",
-        },
-        reactions: {
-            type: DataTypes.JSON,
-            allowNull: false,
-            defaultValue: {},
-        },
-        status: {
-            type: DataTypes.ENUM("draft", "published", "archived"),
-            allowNull: false,
-            defaultValue: "draft",
-        },
-        visibility: {
-            type: DataTypes.ENUM("public", "private"),
-            allowNull: false,
-            defaultValue: "public",
-        },
-        createdAt: {
-            type: DataTypes.DATE,
-            defaultValue: DataTypes.NOW,
-        },
-        updatedAt: {
-            type: DataTypes.DATE,
-            defaultValue: DataTypes.NOW,
-            onUpdate: DataTypes.NOW,
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
     },
-    {
-        sequelize,
-        modelName: "blogs",
-        freezeTableName: true,
-        timestamps: true,
-    }
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    slug: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    category: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "categories",
+        key: "id",
+      },
+      onDelete: "CASCADE",
+    },
+    tags: {
+      type: DataTypes.JSON,
+      allowNull: false,
+      defaultValue: [],
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    featuredImage: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "mu_users",
+        key: "id",
+      },
+      onDelete: "CASCADE",
+    },
+    reactions: {
+      type: DataTypes.JSON,
+      allowNull: false,
+      defaultValue: {},
+    },
+    status: {
+      type: DataTypes.ENUM("draft", "published", "archived"),
+      allowNull: false,
+      defaultValue: "draft",
+    },
+
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      onUpdate: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    modelName: "blogs",
+    freezeTableName: true,
+    timestamps: true,
+  },
 );
 
 export default Blog;
--- src/services/blogs/Blog.service.js
+++ src/services/blogs/Blog.service.js
@@ -6,192 +6,190 @@ import Category from "../../models/category/Category.model.js";
 import UserModel from "../../models/users/User.model.js";
 
 class BlogService {
-    async listBlogs(query = {}) {
-        const page = parseInt(query.page, 10) || 1;
-        const limit = parseInt(query.limit, 10) || 10;
-        const offset = (page - 1) * limit;
+  async listBlogs(query = {}) {
+    const page = parseInt(query.page, 10) || 1;
+    const limit = parseInt(query.limit, 10) || 10;
+    const offset = (page - 1) * limit;
 
-        const search = query.q || "";
-        const categoryFilter = query.category_title || query.category;
-        const authorId = query.author_id;
-        const is_featured = query.is_featured;
-        const status = query.status || "published";
-        const visibility = query.visibility || "public";
+    const search = query.q || "";
+    const categoryFilter = query.category_title || query.category;
+    const authorId = query.author_id;
+    const is_featured = query.is_featured;
+    const status = query.status || "published";
+    const visibility = query.visibility || "public";
 
-        let categoryItem;
-        if (categoryFilter) {
-            categoryItem = await Category.findOne({
-                where: {
-                    [Op.or]: [
-                        { title: categoryFilter },
-                        { slugs: categoryFilter }
-                    ]
-                },
-            });
+    let categoryItem;
+    if (categoryFilter) {
+      categoryItem = await Category.findOne({
+        where: {
+          [Op.or]: [{ title: categoryFilter }, { slugs: categoryFilter }],
+        },
+      });
 
-            if (!categoryItem) {
-                const error = new Error("Category Not Found");
-                error.status = 404;
-                throw error;
-            }
-        }
-
-        const whereCondition = {
-            status,
-            visibility,
-        };
-
-        if (search) {
-            whereCondition.title = { [Op.like]: `%${search}%` };
-        }
-
-        if (categoryItem) {
-            whereCondition.category = categoryItem.id;
-        }
-
-        if (typeof is_featured !== "undefined") {
-            whereCondition.is_featured = is_featured;
-        }
-
-        if (authorId) {
-            whereCondition.author_id = authorId;
-        }
-
-        const { count: totalCount, rows: items } = await Blog.findAndCountAll({
-            where: whereCondition,
-            limit,
-            offset,
-            distinct: true,
-            order: [["createdAt", "DESC"]],
-            include: [
-                {
-                    model: Category,
-                    as: "blogCategory",
-                    attributes: ["id", "title", "slugs"],
-                },
-                {
-                    model: UserModel,
-                    as: "blogAuthor",
-                    attributes: ["firstName", "middleName", "lastName"],
-                },
-            ],
-        });
-
-        return {
-            items,
-            pagination: {
-                currentPage: page,
-                totalPages: Math.ceil(totalCount / limit),
-                limit,
-                totalCount,
-            },
-        };
+      if (!categoryItem) {
+        const error = new Error("Category Not Found");
+        error.status = 404;
+        throw error;
+      }
     }
 
-    async getBlog(identifier) {
-        let whereCondition = {};
-        if (!isNaN(identifier)) {
-            whereCondition = { id: identifier };
-        } else {
-            whereCondition = { slug: identifier };
-        }
+    const whereCondition = {
+      status,
+      visibility,
+    };
 
-        const blog = await Blog.findOne({
-            attributes: {
-                exclude: ["category", "author"],
-            },
-            where: whereCondition,
-            include: [
-                {
-                    model: Category,
-                    as: "blogCategory",
-                    attributes: ["id", "title", "slugs"],
-                },
-                {
-                    model: UserModel,
-                    as: "blogAuthor",
-                    attributes: ["firstName", "middleName", "lastName"],
-                },
-            ],
-        });
-
-        if (!blog) {
-            const error = new Error("Blog not found");
-            error.status = 404;
-            throw error;
-        }
-
-        const similarBlogs = await Blog.findAll({
-            attributes: {
-                exclude: ["category", "author"],
-            },
-            where: {
-                category: blog.blogCategory.id,
-                id: { [Op.ne]: blog.id },
-            },
-            include: [
-                {
-                    model: Category,
-                    as: "blogCategory",
-                    attributes: ["id", "title", "slugs"],
-                },
-                {
-                    model: UserModel,
-                    as: "blogAuthor",
-                    attributes: ["firstName", "middleName", "lastName"],
-                },
-            ],
-            limit: 5,
-        });
-
-        return { blog, similarBlogs };
+    if (search) {
+      whereCondition.title = { [Op.like]: `%${search}%` };
     }
 
-    async createBlog(data) {
-        return Blog.create({
-            ...data,
-            slug: slugify(data.title),
-        });
+    if (categoryItem) {
+      whereCondition.category = categoryItem.id;
     }
 
-    async updateBlog(id, data) {
-        const blog = await Blog.findByPk(id);
-        if (!blog) {
-            const error = new Error("Blog not found");
-            error.status = 404;
-            throw error;
-        }
-
-        let updatedSlug = blog.slug;
-        if (data.title && data.title !== blog.title) {
-            updatedSlug = slugify(data.title);
-        }
-
-        const [updatedRows] = await Blog.update(
-            {
-                ...data,
-                slug: updatedSlug,
-            },
-            { where: { id } }
-        );
-
-        if (updatedRows === 0) {
-            const error = new Error("Blog not found");
-            error.status = 404;
-            throw error;
-        }
-
-        return Blog.findByPk(id);
+    if (typeof is_featured !== "undefined") {
+      whereCondition.is_featured = is_featured;
     }
 
-    async deleteBlog(id) {
-        const deletedRows = await Blog.destroy({ where: { id } });
-        if (deletedRows === 0) {
-            const error = new Error("Blog not found");
-            error.status = 404;
-            throw error;
-        }
+    if (authorId) {
+      whereCondition.author_id = authorId;
     }
+
+    const { count: totalCount, rows: items } = await Blog.findAndCountAll({
+      where: whereCondition,
+      limit,
+      offset,
+      distinct: true,
+      order: [["createdAt", "DESC"]],
+      include: [
+        {
+          model: Category,
+          as: "blogCategory",
+          attributes: ["id", "title", "slugs"],
+        },
+        {
+          model: UserModel,
+          as: "blogAuthor",
+          attributes: ["firstName", "middleName", "lastName"],
+        },
+      ],
+    });
+
+    return {
+      items,
+      pagination: {
+        currentPage: page,
+        totalPages: Math.ceil(totalCount / limit),
+        limit,
+        totalCount,
+      },
+    };
+  }
+
+  async getBlog(identifier) {
+    let whereCondition = {};
+    if (!isNaN(identifier)) {
+      whereCondition = { id: identifier };
+    } else {
+      whereCondition = { slug: identifier };
+    }
+
+    const blog = await Blog.findOne({
+      attributes: {
+        exclude: ["category", "author"],
+      },
+      where: whereCondition,
+      include: [
+        {
+          model: Category,
+          as: "blogCategory",
+          attributes: ["id", "title", "slugs"],
+        },
+        {
+          model: UserModel,
+          as: "blogAuthor",
+          attributes: ["firstName", "middleName", "lastName"],
+        },
+      ],
+    });
+
+    if (!blog) {
+      const error = new Error("Blog not found");
+      error.status = 404;
+      throw error;
+    }
+
+    const similarBlogs = await Blog.findAll({
+      attributes: {
+        exclude: ["category", "author"],
+      },
+      where: {
+        category: blog.blogCategory.id,
+        status: "published",
+        id: { [Op.ne]: blog.id },
+      },
+      include: [
+        {
+          model: Category,
+          as: "blogCategory",
+          attributes: ["id", "title", "slugs"],
+        },
+        {
+          model: UserModel,
+          as: "blogAuthor",
+          attributes: ["firstName", "middleName", "lastName"],
+        },
+      ],
+      limit: 5,
+    });
+
+    return { blog, similarBlogs };
+  }
+
+  async createBlog(data) {
+    return Blog.create({
+      ...data,
+      slug: slugify(data.title),
+    });
+  }
+
+  async updateBlog(id, data) {
+    const blog = await Blog.findByPk(id);
+    if (!blog) {
+      const error = new Error("Blog not found");
+      error.status = 404;
+      throw error;
+    }
+
+    let updatedSlug = blog.slug;
+    if (data.title && data.title !== blog.title) {
+      updatedSlug = slugify(data.title);
+    }
+
+    const [updatedRows] = await Blog.update(
+      {
+        ...data,
+        slug: updatedSlug,
+      },
+      { where: { id } },
+    );
+
+    if (updatedRows === 0) {
+      const error = new Error("Blog not found");
+      error.status = 404;
+      throw error;
+    }
+
+    return Blog.findByPk(id);
+  }
+
+  async deleteBlog(id) {
+    const deletedRows = await Blog.destroy({ where: { id } });
+    if (deletedRows === 0) {
+      const error = new Error("Blog not found");
+      error.status = 404;
+      throw error;
+    }
+  }
 }
 
 export default BlogService;
